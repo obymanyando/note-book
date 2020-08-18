f3ddec20c5ce12d7ad0e46060cc556ac8ed1d4e6
Oby Manyando <33107568+obymanyando@users.noreply.github.com>
2020-08-19 00:31:26 +0300

    initialised amplify project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello, this is CI/CD check.</h1>
-        
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<h1>Hello, this is CI/CD check.</h1>
+				<p>Where did the amp project go?</p>
+			</header>
+		</div>
+	)
 }
 
-export default App;
+export default App
